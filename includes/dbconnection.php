686fc31585c7ee91cf6ab691e20b6f4ecd0f666f
ODUOR FELIX <foduor@ngamia.africa>
2020-08-15 10:58:23 +0300

    first commit

---
+++ includes/dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "Admin", "admin@19920", "bpmsdb");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+  ?>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Beauty Parlour Management System</a>
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link">Services</a></li>
+            
+            <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+           
+            <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link">Admin</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
